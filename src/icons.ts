2f6c00a6ee4384eed5ef975568687d688f6bb115
zhangyu <grewer@grewer.cn>
2018-10-07 22:24:33 +0800

    feat(location): prod 正常 ,不过 有一bug,官方未解决,已添加过度解决方案

---
+++ src/icons.ts
@@ -0,0 +1,5 @@
+export {
+  default as DownOutline
+} from "@ant-design/icons/lib/outline/DownOutline";
+// 需要固定写入自己需要的 icons ,不然会引入所有图标 (大小为500k 作用)
+// 相关连接 https://github.com/ant-design/ant-design/issues/12011#issuecomment-420038579
